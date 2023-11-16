0f0da5290d161fc8110edda38ccfef63327420f9
Surendra <nagavenkatasai7896@gmail.com>
2023-11-16 17:54:38 +0530

    assignment

---
+++ FirstSolution/FirstApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Threading.Channels;
+
+namespace FirstApp
+{
+    internal class Program
+    {
+
+        static void avg()
+        {
+            int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10;
+            Console.WriteLine("Enter 10 number");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+            n3 = Convert.ToInt32(Console.ReadLine());
+            n4 = Convert.ToInt32(Console.ReadLine());
+            n5 = Convert.ToInt32(Console.ReadLine());
+            n6 = Convert.ToInt32(Console.ReadLine());
+            n7 = Convert.ToInt32(Console.ReadLine());
+            n8 = Convert.ToInt32(Console.ReadLine());
+            n9 = Convert.ToInt32(Console.ReadLine());
+            n10 = Convert.ToInt32(Console.ReadLine());
+            int avg = (n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10) / 10;
+            
+                Console.WriteLine("Average is "+ avg);
+           
+
+        }
+        static void Main(string[] args)
+        {
+
+            avg();
+
+        }
+    }
+}
